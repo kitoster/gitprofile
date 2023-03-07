0e18c65d1e0824c79ccc3d869665975e6272d607
Kit Rainier Oster <98862584+kitoster@users.noreply.github.com>
2023-03-07 07:16:22 -0500

    Updated socials list

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -98,7 +98,7 @@ const config = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'dracula',
+    defaultTheme: 'garden',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
